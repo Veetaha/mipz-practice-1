c2a40e768965ae67c80731a054489bdc5da42f4c
Veetaha <veetaha2@gmail.com>
2022-04-23 22:17:57 +0200

    Add sample inputs and fix bugs

--- src/calc.rs
+++ src/calc.rs
@@ -1,8 +1,9 @@
 use serde::Deserialize;
+use std::cell::RefCell;
 use std::collections::HashMap;
 
 // Synthetically limit the number of iterations to avoid infinite cycle
-const MAX_ITER: usize = 1000000000;
+const MAX_ITER: usize = 100000;
 
 type CountriesMap = HashMap<String, CountryDimensions>;
 
@@ -43,15 +44,16 @@ impl MatrixDimensions {
 #[derive(Clone, Default)]
 struct City {
     country: String,
-    coins: HashMap<String, u64>,
+    coins: HashMap<String, i64>,
 }
 
+/// RefCell is required to enable interior mutability
 #[derive(Clone)]
-struct Matrix(Vec<Vec<City>>);
+struct Matrix(Vec<Vec<RefCell<City>>>);
 
 impl Matrix {
     fn new(dim: MatrixDimensions, countries: &CountriesMap) -> Matrix {
-        let mut matrix = vec![vec![City::default(); dim.cols]; dim.rows];
+        let mut matrix = vec![vec![RefCell::<City>::default(); dim.cols + 1]; dim.rows + 1];
 
         // Initialize the matrix based on country coordinates
 
@@ -73,7 +75,7 @@ impl Matrix {
                         city.coins.insert(country_name, coins);
                     }
 
-                    matrix[row][col] = city;
+                    matrix[row][col] = city.into();
                 }
             }
         }
@@ -89,10 +91,7 @@ pub(crate) fn euro_diffusion(countries: &CountriesMap) -> Vec<CountryOutput> {
     imp_euro_diffusion(country_matrix, countries)
 }
 
-pub(crate) fn imp_euro_diffusion(
-    mut country_matrix: Matrix,
-    countries: &CountriesMap,
-) -> Vec<CountryOutput> {
+fn imp_euro_diffusion(country_matrix: Matrix, countries: &CountriesMap) -> Vec<CountryOutput> {
     if countries.len() == 1 {
         return vec![CountryOutput {
             country_name: countries.keys().next().unwrap().clone(),
@@ -100,7 +99,7 @@ pub(crate) fn imp_euro_diffusion(
         }];
     }
 
-    let country_matrix = &mut country_matrix.0;
+    let mut country_matrix = country_matrix.0;
     let transaction_matrix = &mut country_matrix.clone();
 
     let mut iter = 1;
@@ -110,55 +109,54 @@ pub(crate) fn imp_euro_diffusion(
         for row in 0..country_matrix.len() {
             for col in 0..country_matrix[row].len() {
                 let cur_city = &country_matrix[row][col];
-                if cur_city.country.is_empty() {
+                if cur_city.borrow().country.is_empty() {
                     continue;
                 }
 
-                let transaction_cur_city = &mut transaction_matrix[row][col];
+                let transaction_cur_city = &transaction_matrix[row][col];
 
                 let transaction_surrounding_cities = [
-                    transaction_matrix
-                        .get_mut(row - 1)
-                        .and_then(|row| row.get_mut(col)),
-                    transaction_matrix
-                        .get_mut(row)
-                        .and_then(|row| row.get_mut(col - 1)),
-                    transaction_matrix
-                        .get_mut(row)
-                        .and_then(|row| row.get_mut(col + 1)),
-                    transaction_matrix
-                        .get_mut(row + 1)
-                        .and_then(|row| row.get_mut(col)),
+                    transaction_matrix.get(row - 1).and_then(|row| row.get(col)),
+                    transaction_matrix.get(row).and_then(|row| row.get(col - 1)),
+                    transaction_matrix.get(row).and_then(|row| row.get(col + 1)),
+                    transaction_matrix.get(row + 1).and_then(|row| row.get(col)),
                 ]
                 .into_iter()
                 .flatten();
 
                 for transaction_city in transaction_surrounding_cities {
-                    if !transaction_city.country.is_empty() {
+                    if !transaction_city.borrow().country.is_empty() {
                         // Send to surounging cities coins of EACH country depending on city current balance
                         for country_name in countries.keys() {
-                            let value = cur_city.coins[country_name] / 1000;
-                            transaction_city.coins[country_name] += value;
-                            transaction_cur_city.coins[country_name] -= value;
+                            let value = cur_city.borrow().coins[country_name] / 1000;
+                            *transaction_city
+                                .borrow_mut()
+                                .coins
+                                .get_mut(country_name)
+                                .unwrap() += value;
+                            *transaction_cur_city
+                                .borrow_mut()
+                                .coins
+                                .get_mut(country_name)
+                                .unwrap() -= value;
                         }
                     }
                 }
             }
         }
 
-        let country_matrix = transaction_matrix.clone();
+        country_matrix = transaction_matrix.clone();
 
         for country_name in countries.keys().cloned() {
-            let country_cities = country_matrix
+            let country_not_complete = country_matrix
                 .iter()
                 .flatten()
-                .filter(|city| city.country == country_name);
-
-            let country_not_complete = country_cities.any(|city| {
-                countries
-                    .keys()
-                    .any(|country_name| city.coins[country_name] == 0)
-            });
+                .filter(|city| city.borrow().country == country_name)
+                .any(|city| {
+                    countries
+                        .keys()
+                        .any(|country_name| city.borrow().coins[country_name] == 0)
+                });
 
             if !country_not_complete
                 && !completed
